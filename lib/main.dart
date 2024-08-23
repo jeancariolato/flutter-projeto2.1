dd278dbbb5868077b05f17b588275659ef9b09ff
Valhalla <valhallamaia@gmail.com>
2024-08-23 20:09:40 -0300

    Eco App Counter - 23-08-2024

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,55 @@
-
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MyPage());
+void main() {
+  runApp(MyPage());
 }
 
-class MyPage extends StatelessWidget {
-  const MyPage({super.key});
+class MyPage extends StatefulWidget {
+  MyPage({super.key});
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  var _cont = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Meu App",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Meu Aplicativo"),
-          backgroundColor: Colors.cyanAccent[200],
-        ),
-      ),
+          appBar: AppBar(
+            leading: Icon(Icons.eco),
+            title: Text("ECO"),
+            backgroundColor: Colors.green,
+          ),
+          body: Center(
+            child: Container(
+              padding: EdgeInsets.all(40),
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.green)),
+              child: Text(
+                "$_cont",
+                style: TextStyle(fontSize: 30.0, color: Colors.green),
+              ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(
+              Icons.upload_file_outlined,
+              color: Colors.white,
+            ),
+            shape: CircleBorder(),
+            backgroundColor: Colors.green,
+            onPressed: () {
+              setState(() {
+                _cont += 1;
+              });
+              print(_cont);
+            },
+          )),
     );
   }
 }
